ec3dc7016e3cea7f4fadc709a60b645e59cb9a4c
Ryan <rblf100@gmail.com>
2020-05-26 17:31:57 -0400

    Initial addition

---
+++ src/main/java/com/pluralsight/conf2makerdemo/Conf2MakerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conf2makerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Conf2MakerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Conf2MakerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/conf2makerdemo/Conf2MakerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conf2makerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Conf2MakerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
